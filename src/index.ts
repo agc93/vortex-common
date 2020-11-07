1ae2cc06eb7daa9eabd5621df1fd324c38676f19
Alistair Chapman <alistair@agchapman.com>
2020-11-07 17:56:11 +1000

    actually export getModType this time

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { ProfileClient } from './profileClient';
 export { UnrealGameHelper }  from './unreal';
-export {isActiveGame, toAttributeInstructions, getCategoryName, getModName} from './util';
+export {isActiveGame, toAttributeInstructions, getCategoryName, getModName, getModType} from './util';
