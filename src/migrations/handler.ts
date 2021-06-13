aa15e821ab52caa86e7243e1c6dc27261b433f9f
Alistair Chapman <alistair@agchapman.com>
2021-06-14 01:46:30 +1000

    Allow undefined game ID in migration handler; This will skip the "has mods" check and run regardless;

--- src/migrations/handler.ts
+++ src/migrations/handler.ts
@@ -13,18 +13,20 @@ import { IGameModTable } from "../events";
  * @param extInfo The extension update this migration is for.
  * @param callback The callback you want to run when this migration is required and run.
  */
-export function migrationHandler(api: IExtensionApi, gameId: string, extInfo: ExtensionUpdateInfo, callback?: (api: IExtensionApi, extInfo: ExtensionUpdateInfo) => Promise<any>): (oldVersion: string) => Promise<any> {
+export function migrationHandler(api: IExtensionApi, gameId: string|undefined, extInfo: ExtensionUpdateInfo, callback?: (api: IExtensionApi, extInfo: ExtensionUpdateInfo) => Promise<any>): (oldVersion: string) => Promise<any> {
     return (oldVersion) => {
         if (semver.neq(oldVersion, '0.0.0') && (semver.gte(oldVersion, extInfo.newVersion))) {
             return Promise.resolve();
         }
     
         const state = api.store.getState();
-        const mods = util.getSafe<IGameModTable>(state, ['persistent', 'mods', gameId], {});
-        const hasMods = Object.keys(mods).length > 0;
-    
-        if (!hasMods) {
-            return Promise.resolve();
+        if (gameId && gameId !== 'vortex') {
+            const mods = util.getSafe<IGameModTable>(state, ['persistent', 'mods', gameId], {});
+            const hasMods = Object.keys(mods).length > 0;
+        
+            if (!hasMods) {
+                return Promise.resolve();
+            }
         }
 
         extInfo.oldVersion = oldVersion;
